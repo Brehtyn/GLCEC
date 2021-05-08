84c313dc819ca4f23f2e5b115dd2bb323a6b2241
Brehtyn <66739729+Brehtyn@users.noreply.github.com>
2021-05-08 00:48:01 -0700

    php update
    it worked!!
    php to other pages and test of privateform

--- contactForm.php
+++ contactForm.php
@@ -5,7 +5,7 @@ $phone = $_POST['phone'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-$email_from = "brent.hanna@gmail.com";
+$email_from = "admin@glcec.org";
 
 $to = "brent.hanna@gmail.com";
 
@@ -15,7 +15,7 @@ $headers = "From: $email_from \r\n";
 
 $headers .= "Reply-To: $visitor_email \r\n";
 
-$email_body = "You have received a new message: $message".
+$email_body = "You have received a new message from $name (email $visitor_email). \r\n Here is the message: \r\n $message";
 
 mail($to,$email_subject,$email_body,$headers);
 
--- privateForm.php
+++ privateForm.php
@@ -1,5 +1,22 @@
 <?php
-  $name = $_POST['name'];
-  $visitor_email = $_POST['email'];
-  $message = $_POST['message'];
+
+$name = $_POST['private_name'];
+$phone = $_POST['private_phone'];
+$visitor_email = $_POST['private_email'];
+$message = $_POST['private_message'];
+
+$email_from = "admin@glcec.org";
+
+$to = "brent.hanna@gmail.com";
+
+$email_subject = "Contact Form from GLCEC.org";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+$email_body = "You have received a private concern from $name (email $visitor_email). \r\n Here is the message: \r\n $message";
+
+mail($to,$email_subject,$email_body,$headers);
+
 ?>
